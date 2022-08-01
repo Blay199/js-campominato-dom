7149d9601216b03b185e402e700136417eef5791
Blay199 <a.sorrentino03@gmail.com>
2022-08-01 23:39:03 +0200

    mi sono bloccato

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@ getBtnElement.addEventListener('click', grid)
 
 // grid(gridDim, containerElement)
 
-let bombs = []
-
 function grid() {
     let dim = 10;    
     let container = document.querySelector('.container');
     container.innerHTML = '';
     const cellNum = dim ** 2;
+
+    bombsPosition = bombsGenerator(cellNum)
     
     for (let i = 0; i < cellNum; i++) {
         // console.log(i);
@@ -35,4 +35,25 @@ function clickHandler(
     const cell = this;
     cell.classList.toggle('clicked');
     cell.removeEventListener('click', clickHandler);
+}
+
+function bombsGenerator (max) {
+    const bombs = []
+    console.log (bombs)
+
+    while (bombs.length < 16) {
+        const n = getRandomIntInclusive(1, max)
+    }
+
+    if (!bombs.includes(n)) {
+        bombs.push(n)
+    }
+
+    return bombs
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
